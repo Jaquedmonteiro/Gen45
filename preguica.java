b7ee6be11b994234a5bb5be6aee119a6ce1c17f2
Jaque Domingues <jaquem.rs@hotmail.com>
2022-02-03 11:59:17 -0300

    Java

---
+++ preguica.java
@@ -0,0 +1,14 @@
+package polimorfismo;
+
+public class preguica  extends animais{
+	
+	@Override
+	public void emitirSom() {
+    System.out.println("Preguiça zZzZz...");
+	}
+  
+	public void subir() {
+		System.out.println("Preguiça subindo em árvore.");
+	}
+
+}
